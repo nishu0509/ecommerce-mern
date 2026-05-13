fe3384a225081df449cab545d51755874e1bd90f
Nishu <nishuyadav18092005@gmail.com>
2026-05-13 05:45:15 +0530

    update CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(express.json());
-app.use(cookieParser());
+app.use(cors({ 
+  origin: ['http://localhost:5173', 'https://ecommerce-mern.vercel.app'], 
+  credentials: true 
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
