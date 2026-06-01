46f4c4dcbfafd2b41f3af1a47f8115edbd61b138
Nishu <nishuyadav18092005@gmail.com>
2026-06-01 16:36:13 +0530

    Fix CORS allow all origins

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://ecommerce-mern-chi-pink.vercel.app'
-  ],
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 
 app.get('/', (req, res) => {
