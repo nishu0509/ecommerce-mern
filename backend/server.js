aafa52043629810b1f77f8587d94c3b81ecb2934
Nishu <nishuyadav18092005@gmail.com>
2026-06-01 17:48:25 +0530

    Fix CORS regex for all vercal URLs

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'https://ecommerce-mern-chi-pink.vercel.app',
+  origin: /\.vercel\.app$/,
   credentials: true
 }));
 
