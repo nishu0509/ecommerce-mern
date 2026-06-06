c3c5f7007f6b390614d9aedab7845d568d1cb631
Nishu <nishuyadav18092005@gmail.com>
2026-06-07 00:23:31 +0530

    Fix cookie sameSite for cross-origin

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,7 +8,7 @@ const generateToken = (res, id) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
